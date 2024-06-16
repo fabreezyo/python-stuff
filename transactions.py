7005e2f415edeb2e05f4f2b2fbe13b842eb46202
fabreezyo <lylesfabrizio@gmail.com>
2024-06-16 13:58:01 -0400

    Update transactions.py
    formatting

--- transactions.py
+++ transactions.py
@@ -4,7 +4,7 @@ import numpy as np
 def show_df(df: pd.DataFrame, rows=None) -> None:
     with pd.option_context('display.max_rows', rows, 
                            'display.max_columns', None):
-        display(df)
+        display(df) # or print()
            
 def show_groupby(df: pd.DataFrame.groupby) -> None:
     for group, items in df:
@@ -12,6 +12,11 @@ def show_groupby(df: pd.DataFrame.groupby) -> None:
         print(f"\nGroup -> Week starting {group.strftime('%B %d, %Y')}\n")
         show_df(items.reset_index(drop=True))
         print()
+
+def show_dates(df):
+    new_dates = df.Date.map(lambda date: date.strftime('%A, %B %d, %Y'))
+    df.insert(loc=1, column='new_dates', value=new_dates)
+    return df
         
 RENAME_ITEMS = {'7-Eleven': 'Auto & Transport',
                  'Sc state treasur': 'Income',
@@ -26,7 +31,8 @@ transactions = (pd
                 .read_csv('transactions.csv', 
                           usecols=[0,2,3,4,6,8,9,10,11],
                           parse_dates=['Date'],
-                          dtype_backend='numpy_nullable'
+                          converters={'Amount': lambda x: -float(x)},
+                          dtype_backend='numpy_nullable',
                           )
                 )
 
@@ -35,23 +41,23 @@ for find, replace in RENAME_ITEMS.items():
 
 transactions.Category = (np
                          .where(
-                             (transactions.Category == 'Cash & Checks') & (transactions.Amount >= 400.),
-                             'Bills & Utilities',
-                             transactions.Category,
-                             )    
+	                             (transactions.Category == 'Cash & Checks') & (transactions.Amount >= 400.),
+	                             'Bills & Utilities',
+	                             transactions.Category,
+	                             )    
                          )
 
 filter_date_and_accounts: bool = (
-                            (transactions.Date.between('2024-02-01','2024-04-30')) &
-                            (transactions['Description'] != 'Card Payment from Secured Account')
-                            )
+									(transactions.Date.between('2024-02-01','2024-04-30')) &
+									(transactions['Description'] != 'Card Payment from Secured Account')
+									)
 
-filter_transactions: bool = ~(
-                        transactions['Category'].isin(['Internal Transfers', 'Savings Transfer']) &
-                        transactions.Name.str.contains('Transfer|Pending',case=False, regex=True) &
-                        transactions['Account Name'].isin(['Rainy Day', 'Chime Savings'])
-                        )
-    
+filter_transactions: bool = (
+								~transactions['Category'].isin(['Internal Transfers', 'Savings Transfer']) &
+								~transactions.Name.str.contains('Transfer|Pending',case=False, regex=True) &
+								~transactions['Account Name'].isin(['Rainy Day', 'Chime Savings'])
+                            	)
+                         
 last_quarter: pd.DataFrame = (transactions
                 .loc[filter_date_and_accounts & filter_transactions]
                 .dropna(how='all', axis=1)
@@ -64,7 +70,7 @@ weekly_bills = (last_quarter
                                   .sort_values(by='Amount', ascending=False)
                                   .groupby('Category')
                                   .agg({'Amount': 'sum'})
-                                  .sort_values('Amount',ascending=False)
+                                  .sort_values('Amount', ascending=False)
                                   )
                        )
                 )
@@ -74,21 +80,29 @@ monthly_bills = (last_quarter
                  .apply(lambda x: (x
                                    .groupby('Category')
                                    .agg({'Amount': 'sum'})
-                                   .sort_values('Amount',ascending=False)
+                                   .sort_values('Amount', ascending=False)
                                    )
                         )
                  )
 
 average_monthly_income = (last_quarter
-                          .loc[
-                              (last_quarter['Category'] == 'Income')
-                              ]
+                          .loc[last_quarter['Category'] == 'Income']
                           .groupby(pd.Grouper(key='Date', freq='M'))
                           .apply(lambda x: (x
                                             .groupby('Category')
                                             .agg({'Amount': 'sum'})
-                                            .sort_values('Amount',ascending=False)
+                                            .sort_values('Amount', ascending=False)
                                             )
                                  )
                            .mean()
                           )
+
+weekly_transactions_df = (last_quarter
+						  .pipe(show_dates)
+						  .groupby(pd.Grouper(key='Date', freq='W-WED'))
+						  .apply(lambda x: (x
+											.drop(columns=['Date'])
+											.reset_index(drop=True)
+										   )
+								)
+						 )
